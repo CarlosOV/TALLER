124b6691c77306ed9539af37c59344d055d848b8
Brayan Reyes <bcrg.1993@gmail.com>
2015-07-02 15:10:12 -0500

    LoginTerminado ;)

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -5,7 +5,8 @@ import play.mvc.*;
 import views.html.*;
 import play.data.*;
 
-import models.Login;
+import models.*;
+
 public class Home extends Controller {
 
     public Result login(){
@@ -15,6 +16,20 @@ public class Home extends Controller {
 
     public Result verificarUser(){
     	Form<Login> formulario = new Form(Login.class).bindFromRequest();
-    	return ok(login.render(formulario));
+    	boolean login;
+
+    	if(formulario.get().getTipo() == 1){
+    		login = TutorController.loginTutor(formulario.get().getUser_name(), formulario.get().getPassword());
+    		if(login){
+    			return redirect(routes.TutorController.menu());
+    		}
+    	}
+    	else if(formulario.get().getTipo() == 2){
+    		login = AdminController.loginAdministrador(formulario.get().getUser_name(), formulario.get().getPassword());
+    		if(login){
+    			return redirect(routes.AdminController.menu());
+    		}
+    	}
+    	return redirect(routes.Home.login());
     }
 }
