fcaf364f12d0933b150d9161f7b58b75c5d6afc2
cjudiht <cluny02@gmail.com>
2015-07-18 09:40:35 -0500

    commit :3

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -13,8 +13,10 @@ $(document).ready(function(){
         var index = $('#user_type').val();
         if(index == 1){
             $('#btnLogin').attr('formaction', "/tutor/tutorMenu");
+            alert("Es 1");
         }else{
             $('#btnLogin').attr('formaction', "/administrator/adminMenu");
+            alert("Es 2");
         }
     }); 
 
